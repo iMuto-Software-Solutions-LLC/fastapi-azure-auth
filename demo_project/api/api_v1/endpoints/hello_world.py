7ac5f65f937801d381e658cc0ae938bcea526eb6
Josh Mize <jgmize@gmail.com>
2025-08-04 22:09:00 -0500

    Switch hello_world dependencies to azure_scheme

--- demo_project/api/api_v1/endpoints/hello_world.py
+++ demo_project/api/api_v1/endpoints/hello_world.py
@@ -1,6 +1,6 @@
 from typing import Dict, Union
 
-from demo_project.api.dependencies import validate_is_admin_user
+from demo_project.api.dependencies import azure_scheme
 from demo_project.schemas.hello_world import HelloWorldResponse
 from fastapi import APIRouter, Depends, Request
 
@@ -15,7 +15,7 @@ router = APIRouter()
     summary='Say hello',
     name='hello_world',
     operation_id='helloWorld',
-    dependencies=[Depends(validate_is_admin_user)],
+    dependencies=[Depends(azure_scheme)],
 )
 async def world(request: Request) -> Dict[str, Union[str, User]]:
     """
